09d8d727a14c74e7d960e9160c7334b1f8c23d70
jaro <julian.rodriguezoc@campusucc.edu.co>
2024-09-14 10:52:39 -0500

    asd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar'
+import Script from 'next/script'
 
 export default function RootLayout({
   children,
@@ -11,6 +12,7 @@ export default function RootLayout({
     <html lang="es">
       <head>
         <title>CineJaro</title>
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" />
       </head>
       <body className="bg-custom">
         <Navbar />
