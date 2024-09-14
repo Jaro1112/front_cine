94dfdfc3fc6d94e27343824037ac69f6b7e4de03
jaro <julian.rodriguezoc@campusucc.edu.co>
2024-09-14 09:52:14 -0500

    asd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Link from 'next/link'
+import Navbar from './components/Navbar'
 
 export default function RootLayout({
   children,
@@ -11,38 +11,9 @@ export default function RootLayout({
     <html lang="es">
       <head>
         <title>CineJaro</title>
-        <style>{`
-          body {
-            background-color: #071e3d;
-            color: #ffffff;
-          }
-        `}</style>
       </head>
       <body>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="container">
-            <Link href="/" className="navbar-brand">CineJaro</Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <Link href="/peliculas" className="nav-link">Películas</Link>
-                </li>
-                <li className="nav-item">
-                  <Link href="/peliculas/crear" className="nav-link">Crear Película</Link>
-                </li>
-                <li className="nav-item">
-                  <Link href="/series" className="nav-link">Series</Link>
-                </li>
-                <li className="nav-item">
-                  <Link href="/series/crear" className="nav-link">Crear Serie</Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+        <Navbar />
         <main className="container mt-4">
           {children}
         </main>
