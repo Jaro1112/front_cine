a90712890621383d580b2d3c50b556b6384a320b
jaro <julian.rodriguezoc@campusucc.edu.co>
2024-09-14 05:43:48 -0500

    ASdsA

--- src/app/peliculas/crear/page.tsx
+++ src/app/peliculas/crear/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import { peliculaService } from '../../services/peliculaService'
 import PeliculaForm from '../../components/PeliculaForm'
@@ -9,11 +9,18 @@ import { Pelicula } from '../../types/pelicula'
 export default function CrearPelicula() {
   const router = useRouter()
   const [error, setError] = useState<string | null>(null)
+  const [isRouterReady, setIsRouterReady] = useState(false)
+
+  useEffect(() => {
+    setIsRouterReady(true)
+  }, [])
 
   const handleSubmit = async (pelicula: Pelicula) => {
     try {
       await peliculaService.crearPelicula(pelicula)
-      router.push('/peliculas')
+      if (isRouterReady) {
+        router.push('/peliculas')
+      }
     } catch (error) {
       setError('Error al crear la película')
       console.error('Error al crear la película:', error)
