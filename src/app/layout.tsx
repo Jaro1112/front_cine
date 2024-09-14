005c4aac0fd61855d7d260e180daf4caa0348a83
jaro <julian.rodriguezoc@campusucc.edu.co>
2024-09-14 08:32:07 -0500

    asds

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
       <body>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container">
-            <Link href="/" className="navbar-brand">MyCineAvg</Link>
+            <Link href="/" className="navbar-brand">CineJaro</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
