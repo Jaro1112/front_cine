d0179f3744a5f5fa903ba94e7f304a6850b2effe
jaro <julian.rodriguezoc@campusucc.edu.co>
2024-09-14 09:43:31 -0500

    asd

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-custom">
       <div className="container-fluid">
         <Link href="/" className="navbar-brand">CineJaro</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
